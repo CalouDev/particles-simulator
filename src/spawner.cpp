787348a8ab34540a465e743b352e6e1f0b0ae9a4
CalouDev <calixtemansion26@gmail.com>
2025-07-13 22:35:25 +0200

    Added mouse coordinate on 8 pixels grid

--- src/spawner.cpp
+++ src/spawner.cpp
@@ -4,16 +4,18 @@
 void ParticlesManager::eventHandler(sf::Vector2f mouse_coords, sf::Vector2f previous_mouse_coords, ParticlesType particle) {
     if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
         interpolateParticles(particle, mouse_coords, previous_mouse_coords);
-    } else if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Right)) { // Changer dans le futur par l'appui sur un bouton
+    } else if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Right)) {
         manager.clear();
     }
 }
 
 void ParticlesManager::interpolateParticles(ParticlesType particle, sf::Vector2f current_mouse_coords, sf::Vector2f previous_mouse_coords) {
     sf::RectangleShape particle_shape(sf::Vector2f(PARTICLE_SZ, PARTICLE_SZ));
+    current_mouse_coords = sf::Vector2f(std::floor(current_mouse_coords.x / 8) * 8, std::floor(current_mouse_coords.y / 8) * 8);
+    previous_mouse_coords = sf::Vector2f(std::floor(previous_mouse_coords.x / 8) * 8, std::floor(previous_mouse_coords.y / 8) * 8);
     sf::Vector2f delta = current_mouse_coords - previous_mouse_coords;
     float distance = std::sqrt(delta.x * delta.x + delta.y * delta.y);
-    int steps = static_cast<int>(distance / PARTICLE_SZ);
+    int steps = static_cast<int>(distance / (PARTICLE_SZ/2));
     switch (particle) {
     case GroundType:
         addParticles(Ground(particle_shape, current_mouse_coords));
@@ -28,6 +30,7 @@ void ParticlesManager::interpolateParticles(ParticlesType particle, sf::Vector2f
     for (int i = 0; i <= steps; ++i) {
         float t = static_cast<float>(i) / steps;
         sf::Vector2f interpolated_pos = previous_mouse_coords + delta * t;
+        interpolated_pos = sf::Vector2f(std::floor(interpolated_pos.x / 8) * 8, std::floor(interpolated_pos.y / 8) * 8);
         switch (particle) {
             case GroundType:
                 addParticles(Ground(particle_shape, interpolated_pos));
